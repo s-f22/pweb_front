ef02bc480e3800f46c55dc0e6df066d4601c3e48
Samuel <samuel.figs@gmail.com>
2022-03-28 11:17:21 -0300

    pweb_7

--- src/Pages/Home_Vitrine.js
+++ src/Pages/Home_Vitrine.js
@@ -1,6 +1,6 @@
-import oferta_1 from '../Assets/img/carousel_Ofertas_1.jpg'
-import oferta_2 from '../Assets/img/carousel_Ofertas_2.jpg'
-import oferta_3 from '../Assets/img/carousel_Ofertas_3.jpg'
+import oferta1 from '../Assets/img/oferta1.png'
+import oferta2 from '../Assets/img/oferta2.png'
+import oferta3 from '../Assets/img/oferta3.png'
 
 import '../Assets/css/estilos.css';
 import BoxCards from '../Components/BoxCards';
@@ -15,6 +15,44 @@ function Home_Vitrine() {
     <div className='Home_Vitrine_Background'>
       <Cabecalho />
       <MenuCentral />
+
+      <Carousel >
+        <Carousel.Item >
+          <img
+            className="d-block w-100 HomeVitrine_Carousel"
+            src={oferta1}
+            alt="First slide"
+          />
+          <Carousel.Caption className='HomeVitrine_CarrouselTextos'>
+            <h3>Livros usados</h3>
+            <p>Vários títulos com descontos para você aproveitar aquela raridade quem alguem deixou de lado e tá só acumulando poeira.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 HomeVitrine_Carousel"
+            src={oferta2}
+            alt="Second slide"
+          />
+
+          <Carousel.Caption className='HomeVitrine_CarrouselTextos'>
+            <h3>Coleção COMPLETA Stephen King</h3>
+            <p>Todos os volumes do mestre do terror com desconto de 20% em compra única do lote, ou 5% em cada título.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 HomeVitrine_Carousel"
+            src={oferta3}
+            alt="Third slide"
+          />
+
+          <Carousel.Caption className='HomeVitrine_CarrouselTextos'>
+            <h3>Os irmãos Karamázov</h3>
+            <p>Clássico do escritos Fiódor Dostoiévski em 2 volumes com tradução direta do russo por Paulo Bezzera, na editora 34. Edição de colecionador limitada a 20 exemplares.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
 
       <BoxCards />
 
